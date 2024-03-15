805877172862d1a54a51fa3553789755dc69a1a2
cesariin <105024647+cesariin@users.noreply.github.com>
2024-03-14 21:31:08 -0600

    Update CalculadoraTest.java

--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class CalculadoraTest {
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class CalculadoraTest {
+
+    @Test
+    public void testSuma() {
+        Calculadora calc = new Calculadora();
+        assertEquals(5, calc.division(6, 3), calc.suma(2, 3));
+        assertEquals(-1, calc.division(6, 3), calc.suma(-2, 1));
+        assertEquals(0, calc.division(6, 3), calc.suma(0, 0));
+    }
+
+    private void assertEquals(int i, double division, int suma) {
+
+    }
+
+    @Test
+    public void testResta() {
+        Calculadora calc = new Calculadora();
+        assertEquals(1, calc.division(6, 3), calc.resta(3, 2));
+        assertEquals(-3, calc.division(6, 3), calc.resta(2, 5));
+        assertEquals(0, calc.division(6, 3), calc.resta(10, 10));
+    }
+
+    @Test
+    public void testMultiplicacion() {
+        Calculadora calc = new Calculadora();
+        assertEquals(6, calc.division(6, 3), calc.multiplicacion(2, 3));
+        assertEquals(-10, calc.division(6, 3), calc.multiplicacion(2, -5));
+        assertEquals(0, calc.division(6, 3), calc.multiplicacion(0, 10));
+    }
+
+    @Test
+    public void testDivision() {
+        Calculadora calc = new Calculadora();
+        assertEquals(2, calc.division(6, 3), 0);
+        assertEquals(-2, calc.division(6, -3), 0);
+        assertEquals(0, calc.division(0, 10), 0);
+        // Test para división por cero
+        try {
+            calc.division(5, 0);
+            fail("Debería haber lanzado una excepción ArithmeticException");
+        } catch (ArithmeticException e) {
+        }
+    }
 }
+
+
