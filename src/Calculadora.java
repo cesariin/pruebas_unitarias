5b6fe06f83fb2d5e6e9eaec7c1ace1bff84a33fd
cesariin <105024647+cesariin@users.noreply.github.com>
2024-03-14 21:31:32 -0600

    Update Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Calculadora {
+class Calculadora {
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public double division(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return (double) a / b;
+    }
 }
